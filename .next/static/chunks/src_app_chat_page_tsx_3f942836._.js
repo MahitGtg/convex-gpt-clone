51cc4fc016bb0c120ff52bd0bb5619467dc22821
Mahit Gupta <mahit.gupta64@gmail.com>
2025-06-25 11:09:25 +0800

    Remove files now in .gitignore from repository

--- .next/static/chunks/src_app_chat_page_tsx_3f942836._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_c012f259._.js",
-  "static/chunks/_9b3614de._.js"
-],
-    source: "dynamic"
-});
